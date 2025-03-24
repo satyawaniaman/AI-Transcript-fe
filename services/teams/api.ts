c571c27f0cef96951c91ab1521b6dadbcca668ad
Sharathxct <sharathxct@gmail.com>
2025-03-24 05:35:37 +0530

    some mutations and pages

--- services/teams/api.ts
+++ services/teams/api.ts
@@ -1,3 +1,17 @@
+import { api } from "@/utils/axios";
+
 // get all the team for the organisation
 
 // get one team by id
+
+// create a team
+function createTeam(name: string, description: string, organizationId: string) {
+  return api.post('/api/teams', {
+    name,
+    description,
+    organizationId,
+  });
+}
+
+export { createTeam };
+
---
+++ services/teams/mutation.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+import { createTeam } from "./api";
+import { toast } from "react-hot-toast";
+
+function useCreateTeamMutation() {
+  return useMutation({
+    mutationFn: ({ name, description, organizationId }: { name: string, description: string, organizationId: string }) => createTeam(name, description, organizationId),
+    onSuccess: () => {
+      toast.success('Team created successfully');
+    },
+    onError: () => {
+      toast.error('Failed to create team');
+    },
+  });
+}
+
+export default useCreateTeamMutation;
