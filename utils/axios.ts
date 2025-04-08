e21f60e108aea3536007cc0d001f805c527fc7b4
easily-mistaken <choubeyprajjwal@gmail.com>
2025-04-08 15:27:06 +0530

    axios

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,33 +1,39 @@
-import axios, { AxiosInstance, AxiosError, InternalAxiosRequestConfig } from 'axios';
-import { createClient } from '@/utils/supabase/client';
+import axios, {
+  AxiosInstance,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from "axios";
+import { createClient } from "@/utils/supabase/client";
 
 export const api: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/',
-    headers: {
-        'Content-Type': 'application/json',
-    },    
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use(
-    async (config: InternalAxiosRequestConfig) => {
-        try {
-            const supabase = await createClient()
-            const { data: { session } } = await supabase.auth.getSession();
+  async (config: InternalAxiosRequestConfig) => {
+    try {
+      const supabase = await createClient();
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
 
-            if (session?.access_token) {
-                config.headers = config.headers || {};
-                config.headers['Authorization'] = `Bearer ${session.access_token}`;
-            }
+      if (session?.access_token) {
+        config.headers = config.headers || {};
+        config.headers["Authorization"] = `Bearer ${session.access_token}`;
+      }
 
-            return config;
-        } catch (error) {
-            console.error('Error in request interceptor:', error);
-            return config;
-        }
-    },
-    (error: AxiosError) => {
-        return Promise.reject(error);
+      return config;
+    } catch (error) {
+      console.error("Error in request interceptor:", error);
+      return config;
     }
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
